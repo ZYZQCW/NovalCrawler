c4ce0f75a0cdbcb12ad830ae5c2565e630094dee
ZYZQCW <1192031155@qq.com>
2015-11-14 08:25:52 +0800

    Version 0.1 First Commit.

---
+++ MySpider/src/com/zyzq/zuixu/ContentLinkDemo.java
@@ -0,0 +1,61 @@
+package com.zyzq.zuixu;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+/*
+* 过滤小说目录链接的示例代码
+* 
+* 
+*/
+public class ContentLinkDemo {
+	
+    public static void main (String[] args) throws ParserException
+    {
+    	//定义解析器
+        Parser parser = new Parser();
+        URLConnection connection = null;
+        
+        //定义赘婿在笔趣网的目录地址
+		final String adress = "http://www.biquge.tw/1_1287/";
+		
+		try {
+			connection = new URL(adress).openConnection();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		//设置解析器的参数
+        parser.setConnection(connection);
+        parser.setEncoding("utf-8");
+        
+        //定义过滤器一：抓取链接标签
+        NodeFilter filter = new NodeClassFilter (LinkTag.class);
+        //定义过滤器二：抓取标签开头为XXX的标签
+        LinkStartFilter startFilter = new LinkStartFilter(adress);
+        //定义过滤器二：逻辑与过滤器
+        AndFilter andFilter = new AndFilter(filter, startFilter);
+        
+        //执行抓取
+        NodeList links = parser.extractAllNodesThatMatch(andFilter);
+        
+        //遍历抓取结果
+        for (int i = 0; i < links.size (); i++)
+        {
+            LinkTag linkTag = (LinkTag)links.elementAt (i);
+            System.out.print ("\"" + linkTag.getLinkText () + "\" => ");
+            System.out.println (linkTag.getLink ());
+        }
+    }
+}
---
+++ MySpider/src/com/zyzq/zuixu/LinkStartFilter.java
@@ -0,0 +1,46 @@
+package com.zyzq.zuixu;
+
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.tags.LinkTag;
+
+public class LinkStartFilter implements NodeFilter {
+
+	private static final long serialVersionUID = -3689704206995968035L;
+
+	//过滤字符串参数
+	protected String mPattern;
+
+	//标识符，用于判断是否需要大小写
+	protected boolean mCaseSensitive;
+
+	//单参构造函数，不严格要求大小写
+	public LinkStartFilter(String pattern) {
+		this(pattern, false);
+	}
+
+	//双参构造函数，需要指定是否判断大小写
+	public LinkStartFilter(String pattern, boolean caseSensitive) {
+		mPattern = pattern;
+		mCaseSensitive = caseSensitive;
+	}
+
+	//实现接口NodeFilter中的方法
+	public boolean accept(Node node) {
+		boolean ret;
+
+		ret = false;
+		if (LinkTag.class.isAssignableFrom(node.getClass())) {
+			String link = ((LinkTag) node).getLink();
+			if (mCaseSensitive) {
+				if (link.startsWith(mPattern))
+					ret = true;
+			} else {
+				if (link.toUpperCase().startsWith(mPattern.toUpperCase()))
+					ret = true;
+			}
+		}
+
+		return (ret);
+	}
+}
